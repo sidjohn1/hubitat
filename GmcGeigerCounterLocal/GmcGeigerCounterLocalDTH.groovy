006f2366d7f8409390fff1386c27c696b30abfcb
Sidney Johnson <sidjohn1@gmail.com>
2024-02-18 11:07:24 -0600

    Update GmcGeigerCounterLocalDTH.groovy

--- GmcGeigerCounterLocal/GmcGeigerCounterLocalDTH.groovy
+++ GmcGeigerCounterLocal/GmcGeigerCounterLocalDTH.groovy
@@ -1,1 +1,14 @@
+/*
+ * GMC Geiger Counter Local
+ */
 
+metadata {
+	definition(name: "GMC Geiger Counter Local", namespace: "sidjohn1", author: "sidjohn1", importUrl: "https://raw.githubusercontent.com/staze/hubitat-gmcmap/master/hubitat-gmcmap.groovy") {
+		capability "Sensor"
+		attribute "CPM", "NUMBER"                // Count Per Minute reading from this Geiger Counter
+		attribute "ACPM", "NUMBER"               // Average Count Per Minute reading from this Geiger Counter
+		attribute "uSv", "NUMBER"                // uSv/h reading from this Geiger Counter
+        attribute "upload", "string"             // upload responce from gmcmap.com
+        attribute "timestamp", "string"          // Last update
+	}
+}
