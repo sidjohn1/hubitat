23200cff5fed644787ec21fa4c52ea8d686d0c65
Sidney Johnson <sidjohn1@gmail.com>
2024-02-18 11:14:44 -0600

    Update GmcGeigerCounterLocalDTH.groovy

--- GmcGeigerCounterLocal/GmcGeigerCounterLocalDTH.groovy
+++ GmcGeigerCounterLocal/GmcGeigerCounterLocalDTH.groovy
@@ -1,6 +1,23 @@
 /*
- * GMC Geiger Counter Local
- */
+*
+*  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License. You may obtain a copy of the License at:
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+*  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+*  for the specific language governing permissions and limitations under the License.
+*
+*  GMC Geiger Counter Local
+*
+*  Author: Sidney Johnson
+*
+*  Date: 2024-02-18
+*
+*	1.0 - Initial Release
+*
+*/
 
 metadata {
 	definition(name: "GMC Geiger Counter Local", namespace: "sidjohn1", author: "sidjohn1", importUrl: "https://raw.githubusercontent.com/sidjohn1/hubitat/main/GmcGeigerCounterLocal/GmcGeigerCounterLocalDTH.groovy") {
