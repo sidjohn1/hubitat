437835e919eca1ac20c31f3d614ad3595ee60f24
Sidney Johnson <sidjohn1@gmail.com>
2024-03-12 20:47:11 -0500

    v1.0

--- WeatherPanelForAppleShortcuts/WeatherPanelForAppleShortcutsDTH.groovy
+++ WeatherPanelForAppleShortcuts/WeatherPanelForAppleShortcutsDTH.groovy
@@ -9,126 +9,47 @@
 *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
 *  for the specific language governing permissions and limitations under the License.
 *
-*  Forcast Attribute set
+*  Weather Panel For Apple Shortcuts Local Device
 *
 *  Author: Sidney Johnson
 *
-*  Date: 2024-03-09
+*  Date: 2024-03-10
 *
 *	1.0 - Initial Release
 *
 */
 
 metadata {
-    definition (name: "Forcast Attribute set", namespace: "sidjohn1", author: "Sidney Johnson") {
+	definition(
+		name: "Weather Panel For Apple Shortcuts Local Device",
+		description: "Weather Panel For Apple Shortcuts",
+		namespace: "sidjohn1",
+		author: "sidjohn1", 
+		importUrl: "https://raw.githubusercontent.com/sidjohn1/hubitat/main/WeatherPanelForAppleShortcuts/WeatherPanelForAppleShortcutsDTH.groovy"){
+        capability "AirQuality"
+		capability "RelativeHumidityMeasurement"
         capability "Sensor"
-		
-        attribute "currentPrecipAmount", "number"
-	    attribute "currentPrecipChance", "number"
-        attribute "zforecast_1", "string"
-	    attribute "zforecast_2", "string"
-        attribute "zforecast_3", "string"
-	    attribute "zforecast_4", "string"
-        attribute "zforecast_5", "string"
-	    attribute "zforecast_6", "string"
-        attribute "zforecast_7", "string"
-	    attribute "zforecast_8", "string"
-        attribute "zforecast_9", "string"
-	    attribute "zforecast_10", "string"
-        attribute "textDescription", "string"
-        attribute "todaysHigh", "number"
-        attribute "todaysLow", "number"
-
-        command "setZforecast_1", ["string"]
-        command "setZforecast_2", ["string"]
-        command "setZforecast_3", ["string"]
-        command "setZforecast_4", ["string"]
-        command "setZforecast_5", ["string"]
-        command "setZforecast_6", ["string"]
-        command "setZforecast_7", ["string"]
-        command "setZforecast_8", ["string"]
-        command "setZforecast_9", ["string"]
-        command "setZforecast_10", ["string"]
-        command "setTextDescription", ["string"]
-    }   
+        capability "TemperatureMeasurement"
+        
+		attribute "currentCondition", "string"       // Average Count Per Minute reading from this Geiger Counter
+		attribute "currentPrecipAmount", "number"    // uSv/h reading from this Geiger Counter
+        attribute "currentPrecipChance", "number"    // upload responce from gmcmap.com
+        attribute "forcastDay1Day", "string"         // upload responce from gmcmap.com
+        attribute "forcastDay1High", "number"        // upload responce from gmcmap.com
+        attribute "forcastDay1Low", "number"         // upload responce from gmcmap.com
+        attribute "forcastDay1Condition", "string"   // upload responce from gmcmap.com
+        attribute "forcastDay2Day", "string"         // upload responce from gmcmap.com
+        attribute "forcastDay2High", "number"        // upload responce from gmcmap.com
+        attribute "forcastDay2Low", "number"         // upload responce from gmcmap.com
+        attribute "forcastDay2Condition", "string"   // upload responce from gmcmap.com
+        attribute "forcastDay3Day", "string"         // upload responce from gmcmap.com
+        attribute "forcastDay3High", "number"        // upload responce from gmcmap.com
+        attribute "forcastDay3Low", "number"         // upload responce from gmcmap.com
+        attribute "forcastDay3Condition", "string"   // upload responce from gmcmap.com
+        attribute "forcastDay4Day", "string"         // upload responce from gmcmap.com
+        attribute "forcastDay4High", "number"        // upload responce from gmcmap.com
+        attribute "forcastDay4Low", "number"         // upload responce from gmcmap.com
+        attribute "forcastDay4Condition", "string"   // upload responce from gmcmap.com
+        attribute "timestamp", "number"              // Last update
+	}
 }
-
-preferences {
-	input("debugEnable", "bool", title: "Enable debug logging?")
-}
-def setTextDescription(val) {
-    val2 = val.split(':')
-	sendEvent(name: "textDescription", value: val2[0])
-    sendEvent(name: "currentPrecipAmount", value: val2[1])
-    sendEvent(name: "currentPrecipChance", value: val2[2])
-    if(debugEnable) log.debug "set textDescription = $val"
-}
-def setZforecast_1(val) {
-	sendEvent(name: "zforecast_1", value: val)
-    val2 = val.split(':')
-    sendEvent(name: "todaysHigh", value: val2[1].substring(0, val2[1].length() - 2))
-    sendEvent(name: "todaysLow", value: val2[2].substring(0, val2[2].length() - 2))
-    if(debugEnable) log.debug "set zforecast_1 = $val"
-}
-def setZforecast_2(val) {
-	sendEvent(name: "zforecast_2", value: val)
-    if(debugEnable) log.debug "set zforecast_2 = $val"
-}
-def setZforecast_3(val) {
-	sendEvent(name: "zforecast_3", value: val)
-    if(debugEnable) log.debug "set zforecast_3 = $val"
-}
-def setZforecast_4(val) {
-	sendEvent(name: "zforecast_4", value: val)
-    if(debugEnable) log.debug "set zforecast_4 = $val"
-}
-def setZforecast_5(val) {
-	sendEvent(name: "zforecast_5", value: val)
-    if(debugEnable) log.debug "set zforecast_5 = $val"
-}
-def setZforecast_6(val) {
-	sendEvent(name: "zforecast_6", value: val)
-    if(debugEnable) log.debug "set zforecast_6 = $val"
-}
-def setZforecast_7(val) {
-	sendEvent(name: "zforecast_7", value: val)
-    if(debugEnable) log.debug "set zforecast_7 = $val"
-}
-def setZforecast_8(val) {
-	sendEvent(name: "zforecast_8", value: val)
-    if(debugEnable) log.debug "set zforecast_8 = $val"
-}
-def setZforecast_9(val) {
-	sendEvent(name: "zforecast_9", value: val)
-    if(debugEnable) log.debug "set zforecast_9 = $val"
-}
-def setZforecast_10(val) {
-	sendEvent(name: "zforecast_10", value: val)
-    if(debugEnable) log.debug "set zforecast_10 = $val"
-}
-
-def installed() {
-	log.trace "installed()"
-    setTextDescription('')
-    setZforecast_1('')
-    setZforecast_2('')
-    setZforecast_3('')
-    setZforecast_4('')
-    setZforecast_5('')
-    setZforecast_6('')
-    setZforecast_7('')
-    setZforecast_8('')
-    setZforecast_9('')
-    setZforecast_10('')
-}
-
-def updated(){
-	log.trace "updated()"
-	if(debugEnable) runIn(1800,logsOff)
-}
-
-void logsOff() {
-	log.debug "debug logging disabled..."
-	device.updateSetting("debugEnable",[value:"false",type:"bool"])
-}
-
